66f2a7992780cf645c105f26b173c45ef6cf090c
Keerthan2000 <58410481+Keerthan2000@users.noreply.github.com>
2019-12-09 10:54:40 +0530

    Add files via upload

---
+++ goto.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n gitam university");
+    goto a;
+    printf("\n vishakapatnam");
+    printf("\n banglore");
+    a:
+    printf("\n hyderabad");
+    return 0;
+}
