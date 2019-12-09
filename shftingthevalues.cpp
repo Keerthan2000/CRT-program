1df09c5d8d17a2a27af46c11062b9c8dd2ad3a95
Keerthan2000 <58410481+Keerthan2000@users.noreply.github.com>
2019-12-09 10:24:42 +0530

    Add files via upload

---
+++ shftingthevalues.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a=10,b=10;
+    printf("\n a<<2=%d",a<<2);
+     printf("\n b>>2=%d",b>>2);
+     //left shift:
+     //shifting the bits on to the left side
+     //and assigning 0's on to the right side
+    return 0;
+}
